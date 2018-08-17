1cf66d56d8cebb9c09064e1e62dae1b29c4b0337
Matttaylor8910 <taylor.matt777@gmail.com>
2018-08-17 10:37:44 -0600

    Lint

--- src/pages/lokkenisms/lokkenisms.ts
+++ src/pages/lokkenisms/lokkenisms.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { IonicPage, ModalController } from 'ionic-angular';
 import * as _ from 'lodash';
 import { Lokkenism, LokkenismsProvider } from '../../providers/lokkenisms/lokkenisms';
 import { AddLokkenismPage } from '../add-lokkenism/add-lokkenism';
