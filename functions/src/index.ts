64a685d09faa13ec1026f324a372678aa27ed70f
Matttaylor8910 <taylor.matt777@gmail.com>
2018-09-11 15:47:00 -0600

    Add a cloud function to post to slack when lokkenisms are added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,35 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as request from 'request';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// get the admin database
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+const SLACK_URL = functions.config().slack.url;
+
+exports.notifyNewLokkenism = functions.firestore
+    .document('lokkenisms/{lokkenismId}')
+    .onCreate((data, context) => {
+
+        const lokkenismId = context.params.lokkenismId;
+
+        return db.collection('lokkenisms').doc(lokkenismId)
+            .get()
+            .then(doc => {
+
+                const lokkenism = doc.data();
+
+                const author = lokkenism.author ? ' - ' + lokkenism.author : '';
+
+                const message: any = {
+                    text: '_"' + lokkenism.quote + '"_' + author,
+                    mrkdwn: true
+                };
+
+                return request.post(SLACK_URL, { json: message });
+            })
+            .then(() => console.log('posted to slack!!'))
+            .catch(err => console.log(err))
+
+    });
