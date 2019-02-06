0b3213f437d8162784d13c232b85c0a6e14deb7d
Matttaylor8910 <taylor.matt777@gmail.com>
2019-02-06 13:58:23 -0700

    Make the lokkenisms function more succinct

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,26 +11,14 @@ const SLACK_URL = functions.config().slack.url;
 
 exports.notifyNewLokkenism = functions.firestore
     .document('lokkenisms/{lokkenismId}')
-    .onCreate((data, context) => {
-
-        const lokkenismId = context.params.lokkenismId;
-
-        return db.collection('lokkenisms').doc(lokkenismId)
-            .get()
-            .then(doc => {
-
-                const lokkenism = doc.data();
-
-                const author = lokkenism.author ? ' - ' + lokkenism.author : '';
-
-                const message: any = {
-                    text: '_"' + lokkenism.quote + '"_' + author,
-                    mrkdwn: true
-                };
-
-                return request.post(SLACK_URL, { json: message });
-            })
-            .then(() => console.log('posted to slack!!'))
-            .catch(err => console.log(err))
-
+    .onCreate((lokkenismDoc, context) => {
+        const lokkenism = lokkenismDoc.data();
+        const author = lokkenism.author ? ' - ' + lokkenism.author : '';
+        const message: any = {
+            text: '_"' + lokkenism.quote + '"_' + author,
+            mrkdwn: true
+        };
+        console.log('Posting the following to slack: ', message);
+        console.log('Using this URL: ', SLACK_URL);
+        return request.post(SLACK_URL, { json: message });
     });
